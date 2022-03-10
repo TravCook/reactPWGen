0f8228ba550c7ba8285fe1afa7a53f9edb147130
Travis Cook <travismcook93@gmail.com>
2022-03-10 16:06:38 -0700

    added state handling of values for character types

--- src/components/passwordOutput/passwordOutput.js
+++ src/components/passwordOutput/passwordOutput.js
@@ -1,28 +1,85 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./passwordOutput.css"
 
-function passwordOutput(){
+function PasswordOutput(){
+  const [lowerCheck, setlowerCheck] = useState(false)
+  const [upperCheck, setupperCheck] = useState(false)
+  const [numberCheck, setnumberCheck] = useState(false)
+  const [specialCheck, setspecialCheck] = useState(false)
+
+  const handleLCchange = () => {
+    setlowerCheck(!lowerCheck)
+  }
+  const handleUCchange = () => {
+    setupperCheck(!upperCheck)
+  }
+  const handleNumchange = () => {
+    setnumberCheck(!numberCheck)
+  }
+  const handleSpecchange = () => {
+    setspecialCheck(!specialCheck)
+  }
+
+  const handlesubmit = () => {
+    console.log("lower:" + lowerCheck)
+    console.log("upper:" +upperCheck)
+    console.log("number:" +numberCheck)
+    console.log("special:" +specialCheck)
+  }
+
   return (
-    <div className="parentContainer container-fluid">
+    <div className="container-fluid parentContainer">
       <div className="row justify-content-center">
         <div className="col-6 textOutput">Placeholder for now</div>
       </div>
-      <div className="row">
-        <div className="col">Lowercase?</div>
-        <div className="col">Uppercase?</div>
+      <div className="row optionRow">
+        <div className="col">
+          <label>
+            <input 
+              type="checkbox"
+              checked={lowerCheck}
+              onChange={handleLCchange} />
+            Lowercase
+          </label>
+        </div>
+        <div className="col">
+          <label>
+            <input 
+              type="checkbox"
+              checked={upperCheck}
+              onChange={handleUCchange} />
+            Uppercase
+          </label>
+        </div>
       </div>
-      <div className="row">
-        <div className="col">Number?</div>
-        <div className="col">Special Char?</div>
+      <div className="row optionRow">
+        <div className="col">
+          <label>
+            <input 
+              type="checkbox"
+              checked={numberCheck}
+              onChange={handleNumchange} />
+            Number
+          </label>
+        </div>
+        <div className="col">
+          <label>
+            <input 
+              type="checkbox"
+              checked={specialCheck}
+              onChange={handleSpecchange} />
+            Special Character
+          </label>
+        </div>
+      </div>
+      <div className="row optionRow">
+        <div className="col-6">length: </div>
       </div>
       <div className="row justify-content-center">
-        <div className="col-6 textOutput">length: </div>
-      </div>
-      <div className="row justify-content-center">
-        <button className="col-6 ">submit</button>
+        <button className="col-6 btn btn-primary" onClick={handlesubmit}>submit</button>
       </div>
     </div>
   )
 }
 
-export default passwordOutput
+export default PasswordOutput
