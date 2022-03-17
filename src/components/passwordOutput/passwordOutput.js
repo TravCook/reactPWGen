4b73b49dccb35a6bc39f4854334352da77513786
Travis Cook <travismcook93@gmail.com>
2022-03-17 09:24:17 -0600

    logic for password generation is done, only UI left

--- src/components/passwordOutput/passwordOutput.js
+++ src/components/passwordOutput/passwordOutput.js
@@ -8,6 +8,8 @@ function PasswordOutput(){
   const [numberCheck, setnumberCheck] = useState(false)
   const [specialCheck, setspecialCheck] = useState(false)
   const [length, setlength] = useState(0)
+  let passwordArr = []
+  const [password, setPass] = useState("Your password here")
 
   const handleLCchange = () => {
     setlowerCheck(!lowerCheck)
@@ -25,24 +27,45 @@ function PasswordOutput(){
     setlength(e.target.value)
   }
 
+  function getRandom(arr) {
+    var randIndex = Math.floor(Math.random() * arr.length);
+    var randChar = arr[randIndex];
+  
+    return randChar;
+  }
+
   const handlesubmit = () => {
     const uppCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
     const lowCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
     const special = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.']
     const number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
-    console.log("lower: " + lowerCheck)
-    console.log("upper: " +upperCheck)
-    console.log("number: " +numberCheck)
-    console.log("special: " +specialCheck)
-    console.log("Length: "+length)
+    let possibleCharacters = []
+    if(lowerCheck){
+      possibleCharacters = possibleCharacters.concat(lowCase)
+    }
+    if(upperCheck){
+      possibleCharacters = possibleCharacters.concat(uppCase)
+    }
+    if(specialCheck){
+      possibleCharacters = possibleCharacters.concat(special)
+    }
+    if(numberCheck){
+      possibleCharacters = possibleCharacters.concat(number)
+    }
+    console.log(possibleCharacters)
+    console.log("Length: "+ length)
+    for(let i=0; i<length; i++){
+      passwordArr.push(getRandom(possibleCharacters))
+    }
+    console.log(passwordArr)
+    setPass(passwordArr.join(""))
+    
   }
 
   return (
     <div className="container-fluid parentContainer">
       <div className="row justify-content-center">
-        <div className="col-6 textOutput">{
-          
-        }</div>
+        <div className="col-6 textOutput">{password}</div>
       </div>
       <Form>
         <div className="row optionRow">
