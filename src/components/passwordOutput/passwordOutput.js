105ba0cd68c8055bace8664095f360f366f3425c
Travis Cook <travismcook93@gmail.com>
2022-03-17 08:59:42 -0600

    all inputs working and added variables for generator to choose from

--- src/components/passwordOutput/passwordOutput.js
+++ src/components/passwordOutput/passwordOutput.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react'
 import "./passwordOutput.css"
+import Form from "react-bootstrap/Form"
 
 function PasswordOutput(){
   const [lowerCheck, setlowerCheck] = useState(false)
   const [upperCheck, setupperCheck] = useState(false)
   const [numberCheck, setnumberCheck] = useState(false)
   const [specialCheck, setspecialCheck] = useState(false)
+  const [length, setlength] = useState(0)
 
   const handleLCchange = () => {
     setlowerCheck(!lowerCheck)
@@ -19,62 +21,71 @@ function PasswordOutput(){
   const handleSpecchange = () => {
     setspecialCheck(!specialCheck)
   }
+  const handleLengthChange = (e) => {
+    setlength(e.target.value)
+  }
 
   const handlesubmit = () => {
-    console.log("lower:" + lowerCheck)
-    console.log("upper:" +upperCheck)
-    console.log("number:" +numberCheck)
-    console.log("special:" +specialCheck)
+    const uppCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    const lowCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    const special = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.']
+    const number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+    console.log("lower: " + lowerCheck)
+    console.log("upper: " +upperCheck)
+    console.log("number: " +numberCheck)
+    console.log("special: " +specialCheck)
+    console.log("Length: "+length)
   }
 
   return (
     <div className="container-fluid parentContainer">
       <div className="row justify-content-center">
-        <div className="col-6 textOutput">Placeholder for now</div>
+        <div className="col-6 textOutput">{
+          
+        }</div>
       </div>
-      <div className="row optionRow">
-        <div className="col">
-          <label>
-            <input 
+      <Form>
+        <div className="row optionRow">
+          <Form.Group controlId="formLowercase" className="col">
+            <Form.Check 
               type="checkbox"
               checked={lowerCheck}
-              onChange={handleLCchange} />
-            Lowercase
-          </label>
-        </div>
-        <div className="col">
-          <label>
-            <input 
+              onChange={handleLCchange}
+              label="Lowercase Letters" />
+          </Form.Group>
+          <Form.Group className="col">
+            <Form.Check 
               type="checkbox"
               checked={upperCheck}
-              onChange={handleUCchange} />
-            Uppercase
-          </label>
+              onChange={handleUCchange}
+              label="Uppercase Letters" />
+          </Form.Group>
         </div>
-      </div>
-      <div className="row optionRow">
-        <div className="col">
-          <label>
-            <input 
+        <div className="row optionRow">
+          <Form.Group className="col">
+            <Form.Check 
               type="checkbox"
               checked={numberCheck}
-              onChange={handleNumchange} />
-            Number
-          </label>
-        </div>
-        <div className="col">
-          <label>
-            <input 
+              onChange={handleNumchange}
+              label="Numbers" />
+          </Form.Group>
+          <Form.Group className="col">
+            <Form.Check 
               type="checkbox"
               checked={specialCheck}
-              onChange={handleSpecchange} />
-            Special Character
-          </label>
+              onChange={handleSpecchange}
+              label="Special Characters" />
+          </Form.Group>
         </div>
-      </div>
-      <div className="row optionRow">
-        <div className="col-6">length: </div>
-      </div>
+        <Form.Group>
+          <Form.Label>Password Length: </Form.Label>
+          <Form.Control 
+            onChange={handleLengthChange} 
+            type="text" 
+            placeholder="please enter a number between 8 and 128" />
+        </Form.Group>
+      </Form>
+      
       <div className="row justify-content-center">
         <button className="col-6 btn btn-primary" onClick={handlesubmit}>submit</button>
       </div>
